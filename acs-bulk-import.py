23a74f22a7d654aaad1ab5680b76a40eff974019
Andrew Gagne <agagne@gmail.com>
2018-12-31 10:51:51 -0800

    CAB-3384: acs-bulk-import.py should allow specifying individual profi… (#17)
    * CAB-3384: acs-bulk-import.py should allow specifying individual profiles to import

--- acs-bulk-import.py
+++ acs-bulk-import.py
@@ -17,10 +17,11 @@ def getArgs():
     parser.add_argument('-c', '--conf', default='acs.yml', help='conf file')
     parser.add_argument('-b', '--biconf', default='acs-bulk-import.yml', help='bulk import conf file')
     parser.add_argument('-s', '--stage', choices=['dev', 'local', 'test', 'prod'], default='dev')
+    parser.add_argument('-p', '--profile', required='false', help='Limit importing to only the specified profile')
     return parser.parse_args()
 
 #############################################
-def startBulkImport(acsClient, conf):
+def startBulkImport(acsClient, conf, profile):
     # start bulk import
     imports = conf
     if imports:
@@ -28,24 +29,35 @@ def startBulkImport(acsClient, conf):
             sourceDirectoryBase = imp['sourceDirectoryBase']
             sourceDirectories = imp['sourceDirectories']
             targetPath = imp['targetPath']
-            for srcdir in sourceDirectories:
-                sourceDirectory = sourceDirectoryBase + '/' + srcdir
-                logging.info('Uploading content from ' + sourceDirectory + ' to ' + targetPath)
-                acsClient.startBulkImport(sourceDirectory, targetPath)
+            if should_import_profile(profile, targetPath):
+                for srcdir in sourceDirectories:
+                    sourceDirectory = sourceDirectoryBase + '/' + srcdir
+                    logging.info('Uploading content from ' + sourceDirectory + ' to ' + targetPath)
+                    acsClient.startBulkImport(sourceDirectory, targetPath)
 
-                # alfresco allow one bulk import at a time, wait for bulk load to complete
-                status = acsClient.getBulkImportStatus()
-                while status['currentStatus'].lower() != 'idle':
-                    sys.stdout.write('.')
-                    sys.stdout.flush()
-                    time.sleep(1)
+                    # alfresco allow one bulk import at a time, wait for bulk load to complete
                     status = acsClient.getBulkImportStatus()
-                print("")  # new line
+                    while status['currentStatus'].lower() != 'idle':
+                        sys.stdout.write('.')
+                        sys.stdout.flush()
+                        time.sleep(1)
+                        status = acsClient.getBulkImportStatus()
+                    print("")  # new line
 
-                if status['lastResult'].lower() == 'succeeded':
-                    logging.info('Uploaded content from ' + sourceDirectory + ' to ' + targetPath)
-                else:
-                    logging.info('Failed to upload content from ' + sourceDirectory + ' to ' + targetPath)
+                    if status['lastResult'].lower() == 'succeeded':
+                        logging.info('Uploaded content from ' + sourceDirectory + ' to ' + targetPath)
+                    else:
+                        logging.info('Failed to upload content from ' + sourceDirectory + ' to ' + targetPath)
+            else:
+                logging.debug('Skipping Import to "' + targetPath + '" for specified profile: "' +profile + '"')
+
+def should_import_profile(profile, target_path):
+    import_profile = True
+    if profile is not None:
+        target_pieces = target_path.split('/')
+        import_profile = target_pieces[2] == profile
+
+    return import_profile
 
 #############################################
 # main
@@ -54,6 +66,7 @@ def main():
     logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', datefmt='%m/%d/%Y %H:%M:%S',
                         level=logging.INFO
                         )
+
     logging.info('Start ' + sys.argv[0])
     args = getArgs()
     acsClient = AcsClient.fromConfig(args.conf, args.stage)
@@ -61,7 +74,7 @@ def main():
     # load bulk import config file
     biconf = util.getConfig(args.biconf)
 
-    startBulkImport(acsClient, biconf)
+    startBulkImport(acsClient, biconf, args.profile)
 
     logging.info('End ' + sys.argv[0])
 
